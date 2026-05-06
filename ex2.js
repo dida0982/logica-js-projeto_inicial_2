937bc9eb546598986cd2fb30687963bbb5c53443
dida0982 <dida0982@gmail.com>
2026-05-06 20:10:55 -0300

    feat: add function that display in  console a factorial number
    Signed-off-by: dida0982 <dida0982@gmail.com>

--- ex2.js
+++ ex2.js
@@ -1,3 +1,22 @@
 // 2 Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
 
 
+
+function factorialNumber() {
+
+    let number = 5;
+    let result = 1;
+
+    while ( number >= 1 )
+    {
+        console.log( `${number} * ${result} = ${result * number}` );
+
+        result = result * number;
+        number--;
+
+
+    }
+    console.log(`The factorial of 5 is ${result}.`);
+}
+
+factorialNumber();
