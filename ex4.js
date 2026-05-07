19df3e8b594bfa9708fae1cafbac1ccddcf606eb
dida0982 <dida0982@gmail.com>
2026-05-06 22:13:53 -0300

    feat: A function that calculate the area and perimeter of a rectangle.
    Signed-off-by: dida0982 <dida0982@gmail.com>

--- ex4.js
+++ ex4.js
@@ -1,1 +1,17 @@
-// 4 Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando height e largura que serão dadas como parâmetro.
+// 4 Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando length e largura que serão dadas como parâmetro.
+
+// Area of a rectangle = length * width
+// Perimeter = (length*2) + (width*2)
+function areaAndPerimeterOfRectangle() {
+    let width = 4;
+    let length = 3;
+
+    let area = length * width;
+    let perimeter = ( length * 2 ) + ( width * 2 );
+
+    console.log(`The area of a rectangle of ${length}m of length and ${width}m of width is ${area}m² `);
+    console.log(`The perimeter of a rectangle of ${length}m of length and ${width}m of width is ${perimeter}m `);
+
+}
+
+areaAndPerimeterOfRectangle();
