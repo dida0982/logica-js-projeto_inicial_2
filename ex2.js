f359586a44b449251384f0882ddcb7efdadca97a
dida0982 <dida0982@gmail.com>
2026-05-06 21:31:45 -0300

    feat: add function of money converter
    Signed-off-by: dida0982 <dida0982@gmail.com>

--- ex2.js
+++ ex2.js
@@ -1,7 +1,5 @@
 // 2 Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
 
-
-
 function factorialNumber() {
 
     let number = 5;
--- ex3.js
+++ ex3.js
@@ -1,1 +1,19 @@
 // 3 Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.
+
+// Dólar R$4.80 = $1.00 Real
+function moneyConverter() {
+    let dollar = 4.80;
+    let amountOfDollar = 100.00;
+
+    let real = 1.00;
+    let amountOfReal = 100.00;
+
+    let dollarConverter = amountOfDollar * dollar;
+    let realConverter =  amountOfReal / dollar;
+
+    console.log(`If your have ${amountOfDollar} dollars, is it: ${dollarConverter.toFixed(2)} reais`);
+    console.log(`If your have ${amountOfReal} reais, is it: ${realConverter.toFixed(2)} dollar`);
+
+}
+
+moneyConverter();
