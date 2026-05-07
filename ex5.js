0dcf22ac879cf9b0a27f2d0ae7ecbd247a7bc7b2
dida0982 <dida0982@gmail.com>
2026-05-07 09:31:06 -0300

    feat: add function to do perimeter and area of ​​circle
    Signed-off-by: dida0982 <dida0982@gmail.com>

--- ex5.js
+++ ex5.js
@@ -1,1 +1,24 @@
-// 5 Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
+// 5 Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu radius que será fornecido como parâmetro. Considere Pi = 3,14.
+
+// perimeter, radius, pi= 3.14, area.
+// area = 3.14 * radius**2
+// perimeter = 2 * 3.14 * radius
+function perimeterAndAreaOfCircle ( )
+{
+    let pi = 3.14;
+    let radius = 5;
+    let circleOfArea = pi * radius ** 2;
+    let circleOfPerimeter = 2 * pi * radius;
+
+    console.log( `
+        Information:
+        pi = ${pi}
+        radius = ${radius}
+
+        The area of circle is ${circleOfArea.toFixed(2)}cm²
+        The area of perimeter is ${circleOfPerimeter.toFixed(2)}cm
+        `);
+
+}
+
+perimeterAndAreaOfCircle();
